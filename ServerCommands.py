1daa961584878b305a0a49700f91f57a1d48dd82
Connor Bronson <connorbronson@gmail.com>
2019-08-13 17:41:16 -0400

    added abstraction layer for server commands

---
+++ ServerCommands.py
@@ -0,0 +1,13 @@
+class ServerCommands:
+
+    def __init__(self):
+        pass
+
+    def send_command(self):
+        pass
+
+    def broadcast(self, message):
+        print('[SERVER] Broadcasting message: ', message)
+
+    def set_map(self, map_name):
+        print('[SERVER] Setting next map to ',map_name)
