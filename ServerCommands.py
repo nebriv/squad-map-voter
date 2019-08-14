a009aa659afe2af3ef6d12284c823b3c02d77055
Connor Bronson <connorbronson@gmail.com>
2019-08-14 12:46:23 -0400

    implemented bm requests

--- ServerCommands.py
+++ ServerCommands.py
@@ -1,13 +1,31 @@
+import requests
+import time
+import logging
+
 class ServerCommands:
 
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.config = config
+        logging.basicConfig(level=logging.DEBUG, filename='mapvote.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 
-    def send_command(self):
-        pass
+    def send_command(self, command):
+        header = {'Authorization': self.config['MapVoter']['bm_token']}
+        data = {"data":{"type":"rconCommand","attributes":{"command":"raw","options":{"raw":command}}}}
+        try:
+            print('Posting')
+            #requests.post(f'https://api.battlemetrics.com/servers/{self.config['MapVoter']['mb_server_id']}/command', headers=header, json=data)
+        except:
+            time.sleep(1)
+            try:
+                pass
+                #requests.post(f'https://api.battlemetrics.com/servers/{self.server_id}/command', headers=header, json=data)
+            except:
+                logging.error(f'Error sending server command: {command}', exc_info=True)
 
     def broadcast(self, message):
-        print('[SERVER] Broadcasting message: ', message)
+        logging.info(f'[COMMAND] Broadcasting message: {message}')
+        self.send_command(f'AdminBroadcast "{message}"')
 
     def set_map(self, map_name):
-        print('[SERVER] Setting next map to ',map_name)
+        logging.info(f'[COMMAND] Setting next map to: {map_name}')
+        self.send_command(f'AdminSetNextMap "{map_name}"')
