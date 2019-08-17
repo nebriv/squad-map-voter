801e8e72d02d765c83a07d3d16ed40e1eb6b8745
Connor Bronson <connorbronson@gmail.com>
2019-08-17 11:52:36 -0400

    added logging error for no winning map determined

--- main.py
+++ main.py
@@ -114,6 +114,7 @@ class MapVoter:
 
         # do nothing if there is no winning map
         if not winning_map:
+            logging.error('Unable to determine a winning map')
             return
 
         # broadcast winning map
